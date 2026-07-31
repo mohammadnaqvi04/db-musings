ee87f793bbade9d859caaf89eb8baf40c4a5b838
Mohammad Naqvi <neuvard@gmail.com>
2026-07-31 18:20:15 -0400

    destructor working

--- exercises/custom_move.cpp
+++ exercises/custom_move.cpp
@@ -14,26 +14,56 @@
  actually happened, not just a relabeling).
   */
 
-#include <iostream>;
+#include <iostream>
 
 class MyData {
 private:
-  static const int buffer_size = 20;
+  static const int buffer_size = 5;
   int *buffer;
 
 public:
   // Where constructors, overload assignment operators, and destructor lives
 
   // Constructor
-  MyData(int buffer_size) { buffer = new int[buffer_size]; }
+  MyData(int nums[], int buffer_size) {
+    buffer = new int[buffer_size];
 
-  // Copy constructor
+    for (int i = 0; i < buffer_size; i++) {
+      buffer[i] = nums[i];
+    }
+
+    std::cout << "Constructor called" << std::endl;
+  }
+
+  // Copy constructor. Everything we need to copy from is the source, whereas
+  // whatever we need to copy into is the
   MyData(const MyData &source) {
+
+    // allocate data and perform the copy
+    int *buffer = new int[buffer_size];
+
+    for (int i = 0; i < buffer_size; i++) {
+      buffer[i] = source.buffer[i];
+    }
 
     std::cout << "Copy constructor was used" << std::endl;
   }
 
   // Move constructor
+
+  // Move overload assignment called
+
+  // Destructor
+  ~MyData() {
+    // delete[] buffer;
+    std::cout << "Destructor called" << std::endl;
+  }
 };
 
-int main() {}
+int main() {
+
+  int arr[] = {1, 2, 3, 4, 5};
+  MyData instance = MyData(arr, 5);
+
+  MyData otherInstance = instance;
+}
